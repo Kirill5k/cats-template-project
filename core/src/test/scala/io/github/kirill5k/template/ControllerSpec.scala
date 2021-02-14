2673251049c6c757166cd1ad3bdd4e4b2c9f65cd
kirill5k <immotional@aol.com>
2021-02-14 13:19:59 +0000

    controller spec

---
+++ core/src/test/scala/io/github/kirill5k/template/ControllerSpec.scala
@@ -0,0 +1,41 @@
+package io.github.kirill5k.template
+
+import cats.effect.{ContextShift, IO}
+import io.chrisdavenport.log4cats.Logger
+import io.chrisdavenport.log4cats.slf4j.Slf4jLogger
+import io.circe.parser._
+import org.http4s.{Response, Status}
+import org.mockito.{ArgumentMatchersSugar, MockitoSugar}
+import org.scalatest.Assertion
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.http4s.circe._
+
+import scala.concurrent.ExecutionContext
+import scala.io.Source
+
+trait ControllerSpec extends AnyWordSpec with MockitoSugar with ArgumentMatchersSugar with Matchers {
+
+  implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
+  implicit val logger: Logger[IO]   = Slf4jLogger.getLogger[IO]
+
+  def verifyJsonResponse(
+      actual: IO[Response[IO]],
+      expectedStatus: Status,
+      expectedBody: Option[String] = None
+  ): Assertion = {
+    val actualResp = actual.unsafeRunSync()
+
+    actualResp.status must be(expectedStatus)
+    expectedBody match {
+      case Some(expected) =>
+        val actual = actualResp.asJson.unsafeRunSync()
+        actual must be(parse(expected).getOrElse(throw new RuntimeException))
+      case None =>
+        actualResp.body.compile.toVector.unsafeRunSync() mustBe empty
+    }
+  }
+
+  def readFileFromResources(path: String): String =
+    Source.fromResource(path).getLines().toList.mkString
+}
