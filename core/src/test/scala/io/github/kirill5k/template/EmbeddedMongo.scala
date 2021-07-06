a811c8a475e10e8270ff45d8ae837d4c32bf5445
Kirill <immotional@aol.com>
2021-07-06 12:01:20 +0100

    Update EmbeddedMongo.scala

--- core/src/test/scala/io/github/kirill5k/template/EmbeddedMongo.scala
+++ core/src/test/scala/io/github/kirill5k/template/EmbeddedMongo.scala
@@ -5,9 +5,21 @@ import de.flapdoodle.embed.mongo.config.{MongodConfig, Net}
 import de.flapdoodle.embed.mongo.distribution.Version
 import de.flapdoodle.embed.process.runtime.Network
 
+import java.io.File
+import java.nio.file.Files
+
 trait EmbeddedMongo {
 
+  private def clearResources(): Unit = {
+    val tempFile = System.getenv("temp") + File.separator + "extract-" + System.getenv("USERNAME") + "-extractmongod";
+    val extension = if (System.getenv("OS") != null && System.getenv("OS").contains("Windows")) ".exe" else ".sh"
+    Files.deleteIfExists(new File(s"$tempFile$extension").toPath)
+    Files.deleteIfExists(new File(tempFile + ".pid").toPath)
+    ()
+  }
+  
   def withRunningEmbeddedMongo[A](host: String = "localhost", port: Int = 12345)(test: => A): A = {
+    clearResources()
     val starter          = MongodStarter.getDefaultInstance
     val mongodConfig     = MongodConfig.builder()
       .version(Version.Main.PRODUCTION)
