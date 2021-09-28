e5b186f0d19b67f1d1fa4d97ae709b29e88eed95
kirill5k <immotional@aol.com>
2021-09-28 20:34:10 +0100

    Update scalafmt config

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,28 +1,30 @@
 import sbt._
 
 object Dependencies {
+
   object Versions {
     lazy val mongo4cats = "0.4.0"
-    lazy val pureConfig = "0.15.0"
+    lazy val pureConfig = "0.16.0"
     lazy val circe      = "0.14.1"
-    lazy val sttp       = "3.3.6"
+    lazy val sttp       = "3.3.14"
     lazy val http4s     = "1.0.0-M27"
-    lazy val logback    = "1.2.3"
+    lazy val logback    = "1.2.6"
     lazy val log4cats   = "2.1.1"
 
-    lazy val scalaTest     = "3.2.9"
-    lazy val mockito       = "1.16.37"
+    lazy val scalaTest = "3.2.9"
+    lazy val mockito   = "1.16.42"
   }
 
   object Libraries {
+
     object mongo4cats {
-      lazy val core  = "io.github.kirill5k" %% "mongo4cats-core"  % Versions.mongo4cats
-      lazy val circe = "io.github.kirill5k" %% "mongo4cats-circe" % Versions.mongo4cats
+      lazy val core     = "io.github.kirill5k" %% "mongo4cats-core"     % Versions.mongo4cats
+      lazy val circe    = "io.github.kirill5k" %% "mongo4cats-circe"    % Versions.mongo4cats
       lazy val embedded = "io.github.kirill5k" %% "mongo4cats-embedded" % Versions.mongo4cats
     }
 
     object pureconfig {
-      lazy val core       = "com.github.pureconfig" %% "pureconfig"             % Versions.pureConfig
+      lazy val core = "com.github.pureconfig" %% "pureconfig" % Versions.pureConfig
     }
 
     object logging {
@@ -60,25 +62,27 @@ object Dependencies {
       lazy val all = Seq(core, dsl, server, blaze, circe)
     }
 
-    lazy val scalaTest        = "org.scalatest"      %% "scalatest"                 % Versions.scalaTest
-    lazy val mockitoCore      = "org.mockito"        %% "mockito-scala"             % Versions.mockito
-    lazy val mockitoScalatest = "org.mockito"        %% "mockito-scala-scalatest"   % Versions.mockito
+    lazy val scalaTest        = "org.scalatest" %% "scalatest"               % Versions.scalaTest
+    lazy val mockitoCore      = "org.mockito"   %% "mockito-scala"           % Versions.mockito
+    lazy val mockitoScalatest = "org.mockito"   %% "mockito-scala-scalatest" % Versions.mockito
   }
 
-  lazy val core = Seq(
-    Libraries.mongo4cats.core,
-    Libraries.mongo4cats.circe,
-    Libraries.pureconfig.core
-  ) ++
-    Libraries.circe.all ++
-    Libraries.http4s.all ++
-    Libraries.logging.all ++
-    Libraries.sttp.all
+  lazy val core =
+    Seq(
+      Libraries.mongo4cats.core,
+      Libraries.mongo4cats.circe,
+      Libraries.pureconfig.core
+    ) ++
+      Libraries.circe.all ++
+      Libraries.http4s.all ++
+      Libraries.logging.all ++
+      Libraries.sttp.all
 
   lazy val test = Seq(
-    Libraries.scalaTest        % Test,
-    Libraries.mockitoCore      % Test,
-    Libraries.mockitoScalatest % Test,
-    Libraries.mongo4cats.embedded    % Test
+    Libraries.scalaTest           % Test,
+    Libraries.mockitoCore         % Test,
+    Libraries.mockitoScalatest    % Test,
+    Libraries.mongo4cats.embedded % Test
   )
+
 }
