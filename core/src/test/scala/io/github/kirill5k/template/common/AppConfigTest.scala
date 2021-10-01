59101e1b4509a4c82ce72620b79ff6cec1b64a2a
kirill5k <immotional@aol.com>
2021-10-01 20:16:13 +0100

    Fix test

--- core/src/test/scala/io/github/kirill5k/template/common/AppConfigTest.scala
+++ core/src/test/scala/io/github/kirill5k/template/common/AppConfigTest.scala
@@ -1,6 +1,5 @@
 package io.github.kirill5k.template.common
 
-import cats.effect.{Blocker, IO}
 import io.github.kirill5k.template.CatsSpec
 import io.github.kirill5k.template.common.config.AppConfig
 
@@ -9,10 +8,8 @@ class AppConfigSpec extends CatsSpec {
   "An AppConfig" should {
 
     "load itself from reference.conf" in {
-
-      Blocker[IO].use(AppConfig.load[IO]).unsafeToFuture().map { c =>
-        c.server.host mustBe "0.0.0.0"
-      }
+      val config = AppConfig.load
+      config.server.host mustBe "0.0.0.0"
     }
   }
 }
