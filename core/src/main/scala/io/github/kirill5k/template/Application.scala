fbd0c3f4f50e64fa9898bb5b14b47b2fa7163c3e
kirill5k <immotional@aol.com>
2022-01-02 11:38:07 +0000

    Remove unused imports

--- core/src/main/scala/io/github/kirill5k/template/Application.scala
+++ core/src/main/scala/io/github/kirill5k/template/Application.scala
@@ -4,16 +4,11 @@ import cats.effect.IO
 import cats.effect.IOApp
 import io.github.kirill5k.template.common.config.AppConfig
 import io.github.kirill5k.template.health.Health
-import org.http4s.blaze.server.BlazeServerBuilder
-import org.http4s.implicits.*
-import org.http4s.server.Router
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 object Application extends IOApp.Simple:
-
   given logger: Logger[IO] = Slf4jLogger.getLogger[IO]
-
   override val run: IO[Unit] =
     for
       config <- AppConfig.load[IO]
