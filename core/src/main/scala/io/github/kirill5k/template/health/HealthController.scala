a050bd777ef3bc28a71d41ed8caf77d2840d92c3
kirill5k <immotional@aol.com>
2022-01-11 17:31:58 +0000

    HealthController refactor

--- core/src/main/scala/io/github/kirill5k/template/health/HealthController.scala
+++ core/src/main/scala/io/github/kirill5k/template/health/HealthController.scala
@@ -2,37 +2,41 @@ package io.github.kirill5k.template.health
 
 import cats.Monad
 import cats.effect.Async
+import cats.effect.Ref
+import cats.effect.Temporal
 import cats.syntax.either.*
-import cats.syntax.monad.*
+import cats.syntax.flatMap.*
+import cats.syntax.functor.*
 import io.circe.Codec
+import io.github.kirill5k.template.common.http.Controller
+import java.time.Instant
 import org.http4s.HttpRoutes
 import sttp.capabilities.WebSockets
 import sttp.capabilities.fs2.Fs2Streams
 import sttp.tapir.*
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.http4s.Http4sServerInterpreter
-import io.github.kirill5k.template.common.http.Controller
 
-final class HealthController[F[_]: Async] extends Controller[F] {
+final class HealthController[F[_]: Async](
+    private val startupTime: Ref[F, Instant]
+) extends Controller[F] {
 
   implicit val statusSchema: Schema[HealthController.AppStatus] = Schema.string
 
-  private val statusEndpoint: ServerEndpoint[Any, F] =
-    infallibleEndpoint.get
-      .in("health" / "status")
-      .out(jsonBody[HealthController.AppStatus])
-      .serverLogicPure(_ => HealthController.AppStatus.UP.asRight[Nothing])
+  private val statusEndpoint: ServerEndpoint[Any, F] = infallibleEndpoint
+    .get
+    .in("health" / "status")
+    .out(jsonBody[HealthController.AppStatus])
+    .serverLogicSuccess(req => startupTime.get.map(t => HealthController.AppStatus(t)))
 
   def routes: HttpRoutes[F] = Http4sServerInterpreter[F]().toRoutes(statusEndpoint)
 }
 
-object HealthController {
+object HealthController:
 
-  final case class AppStatus(status: Boolean) derives Codec.AsObject
-
-  object AppStatus:
-    inline def UP: AppStatus = AppStatus(true)
+  final case class AppStatus(startupTime: Instant) derives Codec.AsObject
 
   def make[F[_]: Async]: F[Controller[F]] =
-    Monad[F].pure(new HealthController[F])
-}
+    Temporal[F]
+      .realTimeInstant
+      .flatMap(ts => Ref.of(ts).map(ref => new HealthController[F](ref)))
