f7a82382f3cbe1f3b501eab7c7fc5665972fddf7
kirill5k <immotional@aol.com>
2022-03-06 10:16:45 +0000

    Update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,16 +3,16 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val mongo4cats = "0.4.5"
+    val mongo4cats = "0.4.6"
     val pureConfig = "0.17.1"
     val circe      = "0.14.1"
-    val sttp       = "3.4.1"
+    val sttp       = "3.5.1"
     val http4s     = "0.23.10"
-    val logback    = "1.2.10"
-    val log4cats   = "2.1.1"
-    val tapir      = "0.20.0-M10"
+    val logback    = "1.2.11"
+    val log4cats   = "2.2.0"
+    val tapir      = "0.20.1"
 
-    val scalaTest = "3.2.10"
+    val scalaTest = "3.2.11"
     val mockito   = "3.2.10.0"
   }
 
