c87553a099a5e9c3738c023f8ebb54932eedd138
kirill5k <immotional@aol.com>
2023-05-11 11:49:12 +0100

    Use clock in health controller

--- core/src/main/scala/io/github/kirill5k/template/health/HealthController.scala
+++ core/src/main/scala/io/github/kirill5k/template/health/HealthController.scala
@@ -6,6 +6,8 @@ import cats.syntax.flatMap.*
 import cats.syntax.functor.*
 import io.circe.Codec
 import io.github.kirill5k.template.common.http.Controller
+import io.github.kirill5k.template.common.time.Clock
+import io.github.kirill5k.template.common.time.syntax.*
 import org.http4s.HttpRoutes
 import sttp.tapir.*
 import sttp.tapir.generic.auto.SchemaDerivation
@@ -13,29 +15,53 @@ import sttp.tapir.json.circe.{TapirJsonCirce, jsonBody}
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.http4s.Http4sServerInterpreter
 
+import java.net.InetAddress
 import java.time.Instant
 
 final class HealthController[F[_]: Async](
-    private val startupTime: Instant
+    private val startupTime: Instant,
+    private val ipAddress: String,
+    private val appVersion: Option[String]
+)(using
+    C: Clock[F]
 ) extends Controller[F] {
 
-  private val statusEndpoint: ServerEndpoint[Any, F] = HealthController.statusEndpoint
-    .serverLogicPure(_ => Right(HealthController.AppStatus(startupTime)))
+  private val statusEndpoint: ServerEndpoint[Any, F] =
+    HealthController.statusEndpoint
+      .serverLogicSuccess { _ =>
+        C.durationBetweenNowAnd(startupTime)
+          .map { uptime =>
+            HealthController.Status(
+              startupTime,
+              uptime.toReadableString,
+              appVersion,
+              ipAddress
+            )
+          }
+      }
 
   def routes: HttpRoutes[F] = Http4sServerInterpreter[F]().toRoutes(List(statusEndpoint))
 }
 
 object HealthController extends TapirJsonCirce with SchemaDerivation {
 
-  final case class AppStatus(startupTime: Instant) derives Codec.AsObject
+  final case class Status(
+      startupTime: Instant,
+      upTime: String,
+      appVersion: Option[String],
+      serverIpAddress: String
+  ) derives Codec.AsObject
 
   val basePath = "health"
 
   val statusEndpoint = infallibleEndpoint.get
     .in(basePath / "status")
-    .out(jsonBody[HealthController.AppStatus])
+    .out(jsonBody[HealthController.Status])
 
-  def make[F[_]: Async]: F[Controller[F]] =
-    Temporal[F].realTimeInstant
-      .map(HealthController[F](_))
+  def make[F[_]](using F: Async[F], C: Clock[F]): F[Controller[F]] =
+    for
+      now     <- C.now
+      ip      <- F.delay(InetAddress.getLocalHost.getHostAddress)
+      version <- F.delay(sys.env.get("VERSION"))
+    yield new HealthController[F](now, ip, version)
 }
--- core/src/test/scala/io/github/kirill5k/template/health/HealthControllerSpec.scala
+++ core/src/test/scala/io/github/kirill5k/template/health/HealthControllerSpec.scala
@@ -1,27 +1,42 @@
 package io.github.kirill5k.template.health
 
 import cats.effect.{IO, Ref}
-import io.github.kirill5k.template.ControllerSpec
+import io.github.kirill5k.template.common.time.Clock
+import io.github.kirill5k.template.{ControllerSpec, MockClock}
 import org.http4s.implicits.*
 import org.http4s.*
 import org.http4s.Header.Raw
 import org.typelevel.ci.CIString
 
 import java.time.Instant
+import scala.concurrent.duration.*
 
 class HealthControllerSpec extends ControllerSpec {
 
-  val ts = Instant.parse("2021-01-01T00:00:00Z")
+  val ipAddress = "127.0.0.1"
+  val timestamp = Instant.parse("2020-01-01T00:00:00Z")
+
+  given clock: Clock[IO] = MockClock[IO](timestamp)
 
   "A HealthController" should {
 
     "return status on the app" in {
-      val controller = new HealthController[IO](ts)
+      val controller = new HealthController[IO](timestamp, ipAddress, Some("v0.0.1"))
 
-      val request  = Request[IO](uri = uri"/health/status", method = Method.GET, headers = Headers(Raw(CIString("foo"), "bar")))
-      val response = controller.routes.orNotFound.run(request)
+      val response = for
+        _ <- clock.sleep(1.day + 2.hours + 30.minutes + 10.seconds)
+        req = Request[IO](uri = uri"/health/status", method = Method.GET, headers = Headers(Raw(CIString("foo"), "bar")))
+        res <- controller.routes.orNotFound.run(req)
+      yield res
 
-      response mustHaveStatus (Status.Ok, Some(s"""{"startupTime":"$ts"}"""))
+      val responseBody =
+        s"""{
+           |"startupTime": "$timestamp",
+           |"appVersion": "v0.0.1",
+           |"upTime": "1d2h30m10s",
+           |"serverIpAddress": "$ipAddress"
+           |}""".stripMargin
+      response mustHaveStatus(Status.Ok, Some(responseBody))
     }
   }
 }
