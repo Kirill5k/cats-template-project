6a889a57e8af5ec6df59da4eb90438389c8b2e06
kirill5k <immotional@aol.com>
2023-06-02 17:23:15 +0100

    Bump dependencies

--- core/src/main/scala/io/github/kirill5k/template/Server.scala
+++ core/src/main/scala/io/github/kirill5k/template/Server.scala
@@ -3,6 +3,7 @@ package io.github.kirill5k.template
 import cats.effect.Async
 import com.comcast.ip4s.{Ipv4Address, Port}
 import fs2.Stream
+import fs2.io.net.Network
 import org.http4s.{HttpApp, HttpRoutes}
 import org.http4s.ember.server.EmberServerBuilder
 import io.github.kirill5k.template.common.config.ServerConfig
@@ -11,10 +12,10 @@ import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.*
 
 object Server:
-  def serve[F[_]: Async](config: ServerConfig, routes: HttpApp[F]): Stream[F, Unit] =
+  def serve[F[_]](config: ServerConfig, routes: HttpApp[F])(using F: Async[F]): Stream[F, Unit] =
     Stream.eval {
       EmberServerBuilder
-        .default[F]
+        .default(F, Network.forAsync[F])
         .withHostOption(Ipv4Address.fromString(config.host))
         .withPort(Port.fromInt(config.port).get)
         .withIdleTimeout(1.hour)
